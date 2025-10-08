ab4bf940ccaaa1525c56956ec9b134f244b50210
Viktor Target <viktor.target@gmail.com>
2025-10-08 23:31:09 +0200

    Update vis-klasser.php

--- obligatorisk-oppgave-2/klasser/vis-klasser.php
+++ obligatorisk-oppgave-2/klasser/vis-klasser.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vis alle klasser</title>
     <style>
-        table {border: 1px;}
+        table {
+            border: 1px solid black;
+        }
     </style>
 </head>
 
