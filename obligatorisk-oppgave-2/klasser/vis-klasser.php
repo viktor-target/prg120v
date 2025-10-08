ddb9441eea1a43c02084c2c88d3b7f15001ce43c
Viktor Target <viktor.target@gmail.com>
2025-10-09 00:00:01 +0200

    Update vis-klasser.php

--- obligatorisk-oppgave-2/klasser/vis-klasser.php
+++ obligatorisk-oppgave-2/klasser/vis-klasser.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vis alle klasser</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="obligatorisk-oppgave-2\style.css">
 </head>
 
 <body>
