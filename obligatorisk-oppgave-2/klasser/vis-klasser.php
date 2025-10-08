4899ff664d23dfa57e827acf08b2b655066da481
Viktor Target <viktor.target@gmail.com>
2025-10-08 23:55:10 +0200

    Justerte vis-klasser

--- obligatorisk-oppgave-2/klasser/vis-klasser.php
+++ obligatorisk-oppgave-2/klasser/vis-klasser.php
@@ -5,26 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vis alle klasser</title>
-    <style>
-        table,
-        th,
-        td {
-            border-collapse: collapse;
-            border: 1px solid black;
-            padding: 8px 8px;
-        }
-    </style>
+    <link rel="stylesheet" href="obligatorisk-oppgave-2\style.css">
 </head>
 
 <body>
     <?php
-
-    include("../db-tilkobling.php");
+    include("obligatorisk-oppgave-2\db-tilkobling.php");
 
     $sqlSetning = "SELECT * FROM klasse;";
     $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen.");
     $antallRader = mysqli_num_rows($sqlResultat);
     ?>
+
     <h1>Registrerte klasser</h1>
     <table>
         <tr>
@@ -32,6 +24,7 @@
             <th>Klassenavn</th>
             <th>Studiumkode</th>
         </tr>
+
         <?php
         for ($r = 0; $r < $antallRader; $r++) {
             $rad = mysqli_fetch_array($sqlResultat);
