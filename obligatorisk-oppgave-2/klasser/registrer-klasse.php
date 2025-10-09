36bd587aaa0869e7c4a2ca86b88ec4f922173f08
Viktor Target <viktor.target@gmail.com>
2025-10-09 23:26:03 +0200

    Create registrer-klasse.php

---
+++ obligatorisk-oppgave-2/klasser/registrer-klasse.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Registrer klasse</h1>
+    <form method="post" id="registrerKlasse" name="registrerKlasse">
+        Klassekode<input type="text" id="klassekode" name="klassekode"><br>
+        Klassenavn<input type="text" id="klassenavn" name="klassenavn"><br>
+        Studiumkode<input type="text" id="studiumkode" name="studiumkode"><br>
+        <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp">
+        <input type="reset" value="Nullstill" id="nullstillKnapp" name="nullstillKnapp">
+    </form>
+
+    <?php
+    if (isset($_POST["registrerKlasseKnapp"])) {
+        $klassekode = $_POST["klassekode"];
+        $klassenavn = $_POST["klassenavn"];
+        $studiumkode = $_POST["studiumkode"];
+
+        if (!$klassekode) {
+            echo "Klassekode er ikke fylt ut. <br>";
+        }
+        if (!$klassenavn) {
+            echo "Klassenavn er ikke fylt ut. <br>";
+        }
+        if (!$studiumkode) {
+            echo "Studiumkode er ikke fylt ut. <br>";
+        } else {
+            include("../db-tilkobling.php");
+
+            $sqlSetning = "SELECT * FROM klasse WHERE klassekode = '$klassekode';";
+            $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen.");
+            $antallRader = mysqli_num_rows($sqlResultat);
+
+            if ($antallRader != 0) {
+                echo "Klassekoden er registrert fra før. <br>";
+            } else {
+                $sqlSetning = "INSERT INTO klasse VALUES('$klassekode', '$klassenavn', '$studiumkode');";
+                mysqli_query($db, $sqlSetning) or die("Ikke mulig å registrere data i databasen.");
+
+                echo "Følgende klasse er nå registrert: $klassekode $klassenavn $studiumkode.";
+            }
+        }
+    }
+    ?>
+</body>
+
+</html>
