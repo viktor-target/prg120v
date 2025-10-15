cdc264d5cc9fcd1e08f3076569e9d93cfdf4c1a1
Viktor Target <viktor.target@gmail.com>
2025-10-15 22:42:02 +0200

    Update tre-ganger-tre.php

--- øvingsoppgaver/tema2/tre-ganger-tre.php
+++ øvingsoppgaver/tema2/tre-ganger-tre.php
@@ -11,15 +11,15 @@
     <h1>Tre ganger tre</h1>
     <p>Hva er 3 ganger 3?</p>
     <form method="post" id="treGangerTre" name="treGangerTre">
-        <input type="text" value="lul?" id="svar" name="svar"><br><br>
+        <input type="text" id="svar" name="svar"><br><br>
         <input type="submit" value="Fortsett" id="svarKnapp" name="svarKnapp">
-    </form><br><br>
+    </form><br>
 
     <?php
     if (isset($_POST["svarKnapp"])) {
         $svar = $_POST["svar"];
         if ($svar == 9) {
-            echo "Riktig. 3 ganger 3 er 9";
+            echo "Riktig. 3 ganger 3 er 9.";
         } else {
             echo "Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9.";
         }
