d364b3ccd0571f8d423ff181147df58acbf34994
Viktor Target <viktor.target@gmail.com>
2025-10-17 20:05:29 +0200

    La til oppg2-3

---
+++ øvingsoppgaver/tema2/oppgave2-3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Er du student 2</title>
+</head>
+
+<body>
+    <h1>Er du student? 2</h1>
+    <h2>Svar med j/ja eller n/nei (store eller små bokstaver).</h2>
+    <form method="post" id="erDuStudent" name="erDuStudent">
+        <input type="text" id="svar" name="svar"><br><br>
+        <input type="submit" value="Fortsett" name="svarKnapp" id="svarKnapp">
+    </form><br>
+
+    <?php
+    if (isset($_POST["svarKnapp"])) {
+        $svar = strtolower($_POST["svar"]);
+        if (!$svar) {
+            echo "Du har ikke svart.";            
+        } else if ($svar == "j" || $svar == "ja") {
+            echo "Du har svart ja på spørsmålet om du er student.";
+        } else if ($svar == "n" || $svar == "nei") {
+            echo "Du har svart nei på spørsmålet om du er student.";
+        } else {
+            echo "Du har svart noe annet enn ja/j eller nei/n.";
+        }
+    }
+    ?>
+</body>
+
+</html>
