f15af64e5da5f600ed42647807e9ade83e9c1079
Viktor Target <viktor.target@gmail.com>
2025-10-28 18:35:51 +0100

    Update oppgave2-6.php

--- øvingsoppgaver/tema2/oppgave2-6.php
+++ øvingsoppgaver/tema2/oppgave2-6.php
@@ -17,7 +17,7 @@
         <label for="tall2">Tall 2 </label>
         <input type="text" id="tall2" name="tall2"><br><br>
         <input type="submit" value="Fortsett" id="svarKnapp" name="svarKnapp">
-    </form>
+    </form><br>
 
     <?php
     if (isset($_POST["svarKnapp"])) {
