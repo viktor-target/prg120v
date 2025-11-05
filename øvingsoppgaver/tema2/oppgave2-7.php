ebef6f02b274e1c8839d8ba2a70b903babe999a2
Viktor Target <viktor.target@gmail.com>
2025-11-05 14:45:43 +0100

    La til tallene brukeren skrev inn

--- øvingsoppgaver/tema2/oppgave2-7.php
+++ øvingsoppgaver/tema2/oppgave2-7.php
@@ -38,6 +38,11 @@
         ) {
             echo "Du har skrevet noe annet enn tall.";
         } else {
+            echo "
+            Tall 1 er $tall1<br>
+            Tall 2 er $tall2<br>
+            Tall 3 er $tall3<br><br>";
+            
             if ($tall1 < $tall2) {
                 echo "$tall1 er mindre enn $tall2<br>";
             } else if ($tall1 > $tall2) {
