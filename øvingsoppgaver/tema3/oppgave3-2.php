d15f1e915f4b15f267799b192005339bbfa11a22
Viktor Target <viktor.target@gmail.com>
2025-11-06 10:10:28 +0100

    Endret title og h1

--- øvingsoppgaver/tema3/oppgave3-2.php
+++ øvingsoppgaver/tema3/oppgave3-2.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tall fra 1 til 10</title>
+    <title>Tall og kvadrat fra 1 til 10</title>
 </head>
 
 <body>
-    <h1>Tall fra 1 til 10</h1>
+    <h1>Tall og kvadrat fra 1 til 10</h1>
     <?php
     $antallTall = 10;
     for ($i = 1; $i <= $antallTall; $i++) {
