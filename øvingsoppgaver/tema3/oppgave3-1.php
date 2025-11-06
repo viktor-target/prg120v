c5599d81325bf9b67ad023867baf8f317214c1c3
Viktor Target <viktor.target@gmail.com>
2025-11-06 09:52:16 +0100

    La til oppgave3-1

---
+++ øvingsoppgaver/tema3/oppgave3-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tall fra 1 til 10</title>
+</head>
+
+<body>
+    <h1>Tall fra 1 til 10</h1>
+    <?php
+    $antallTall = 10;
+    for ($i = 1; $i <= $antallTall; $i++) {
+        echo "$i<br>";
+    }
+    ?>
+</body>
+
+</html>
